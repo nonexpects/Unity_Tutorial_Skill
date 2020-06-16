13ed6632594261fd6f96d2dd9a7f10e94321af7a
orionx6x <orionx6x@gmail.com>
2020-06-16 16:33:55 +0900

    1. 오브젝트 풀링 2. HP바

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    Rigidbody m_myrigid = null;
+
+    void OnEnable()
+    {
+        if(m_myrigid == null)
+        {
+            m_myrigid = GetComponent<Rigidbody>();
+        }
+        m_myrigid.velocity = Vector3.zero;
+        m_myrigid.AddExplosionForce(1000, transform.position, 1f);
+
+        StartCoroutine(DestroyCube());
+    }
+    
+    IEnumerator DestroyCube()
+    {
+        yield return new WaitForSeconds(1f);
+        ObjectPoolingManager.instance.InsertQueue(gameObject);
+    }
+}
---
+++ Assets/Scripts/ObjectPoolingManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolingManager : MonoBehaviour
+{
+    public static ObjectPoolingManager instance;
+
+    public GameObject m_goPrefab = null;
+
+    public Queue<GameObject> m_queue = new Queue<GameObject>();
+    void Start()
+    {
+        instance = this;
+        for (int i = 0; i < 500; i++)
+        {
+            GameObject t_object = Instantiate(m_goPrefab, Vector3.zero, Quaternion.identity);
+            m_queue.Enqueue(t_object);
+            t_object.SetActive(false);
+        }
+    }
+
+    public void InsertQueue(GameObject obj)
+    {
+        m_queue.Enqueue(obj);
+        obj.SetActive(false);
+    }
+
+    public GameObject GetQueue()
+    {
+        GameObject t_objecct = m_queue.Dequeue();
+        t_objecct.SetActive(true);
+        return t_objecct;
+    }
+}
